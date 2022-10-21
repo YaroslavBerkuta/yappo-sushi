f1f523315db6eec47201056639ce7f4019522062
Павло Додь <pasha@creatory.studio>
2022-10-21 12:33:19 +0300

    add popups

--- src/js/main.js
+++ src/js/main.js
@@ -5,21 +5,10 @@ flsFunctions.isWebp();
 
 Swiper.use([Mousewheel, Pagination]);
 const swiper = new Swiper(".slider", {
-  // on: {
-  //   init: function () {
-  //     const body = document.getElementsByTagName("body")[0];
-  //     body.classList.add("no-scroll");
-  //   },
-  //   reachEnd: function () {
-  //     const body = document.getElementsByTagName("body")[0];
-  //     body.classList.remove("no-scroll");
-  //   },
-  // },
   pagination: {
     el: ".swiper-pagination",
     type: "bullets",
   },
-  mousewheel: true,
   slidesPerView: 1,
   breakpoints: {
     768: {
@@ -30,13 +19,27 @@ const swiper = new Swiper(".slider", {
   },
 });
 const footetLogo = document.querySelector(".footer__logo");
-const footerSocial = document.querySelector(".footer__social")
-const headerLogo = document.querySelector(".header__logo")
+const footerSocial = document.querySelector(".footer__social");
+const headerLogo = document.querySelector(".header__logo");
 if (window.innerWidth <= 500) {
   footetLogo.remove();
-  document.querySelector(".footer__top").appendChild(footetLogo)
+  document.querySelector(".footer__top").appendChild(footetLogo);
   headerLogo.remove();
-  document.querySelector(".header__top").prepend(headerLogo)
+  document.querySelector(".header__top").prepend(headerLogo);
   footerSocial.remove();
-  document.querySelector(".footer__top").prepend(footerSocial)
+  document.querySelector(".footer__top").prepend(footerSocial);
 }
+
+document
+  .querySelectorAll(".product__item .product__cart .btn")
+  .forEach((btn) => {
+    btn.addEventListener("click", () => {
+      document.querySelector(".cart__wrapper").classList.add("active");
+    });
+  });
+document.querySelector(".cart__item .close").addEventListener("click", () => {
+  document.querySelector(".cart__wrapper").classList.remove("active");
+});
+document.querySelector(".cart").addEventListener("click", () => {
+  document.querySelector(".cart__preview").classList.toggle("active");
+})
